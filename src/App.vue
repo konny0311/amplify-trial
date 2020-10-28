00583940173a44b0c660b26629181351c48155e0
konny0311 <tatsuya.kono.dev@gmail.com>
2020-10-28 14:18:40 +0900

    success user switch

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <amplify-authenticator>
-    <div id="app">
+  <div id="app">
+    <div v-if="signedIn">
       <h1>Todo App</h1>
       <div>Hello, {{user.username}}</div>
       <input type="text" v-model="name" placeholder="Todo name">
       <input type="text" v-model="description" placeholder="Todo description">
       <button v-on:click="createTodo">Create Todo</button>
-    <div v-for="item in todos" :key="item.id">
+      <div v-for="item in todos" :key="item.id">
         <h3>{{ item.name }}</h3>
         <p>{{ item.description }}</p>
       </div>
+      <amplify-sign-out/>
     </div>
-  <amplify-sign-out></amplify-sign-out>
-</amplify-authenticator>
+    <div v-else>
+      <amplify-authenticator />
+    </div>    
+  </div>
 </template>
 
 <script>
@@ -20,12 +23,13 @@ import { API, Auth } from 'aws-amplify';
 import { createTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
 import { onCreateTodo } from './graphql/subscriptions';
-// import { AmplifyEventBus } from 'aws-amplify-vue'
+import { AmplifyEventBus } from 'aws-amplify-vue'
 
 export default {
   name: 'app',
   data() {
     return {
+      signedIn: false,
       user: '',
       name: '',
       description: '',
@@ -33,12 +37,22 @@ export default {
     }
   },
   async beforeCreate() {
-    this.user = await Auth.currentUserInfo()
-    console.log("beforecreate")
-    console.log(this.user.username)
-    // AmplifyEventBus.$on('authState', info => {
-    //   console.log(`Here is the auth event that was just emitted by an Amplify component: ${info}`)
-    // });
+    try {
+      let cognitoUser = await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+      this.user = cognitoUser
+    } catch (err) {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', async  info => {
+      if (info === 'signedIn') {
+        let cognitoUser = await Auth.currentAuthenticatedUser()
+        this.signedIn = true
+        this.user = cognitoUser
+      } else {
+        this.signedIn = false
+      }
+    });    
   },
   created() {
     this.getTodos();
@@ -47,8 +61,15 @@ export default {
   mounted() {
     console.log("mounted")
   },
-  updated() {
+  async updated() {
     console.log("updated")
+    const user =  await Auth.currentUserInfo()
+    const username = user.username
+    const todos = await API.graphql({
+      query: listTodos,
+      variables: {filter: { username: { eq: username } } }, // filter samples=>https://docs.amplify.aws/cli/graphql-transformer/examples#blog-queries
+    });
+    this.todos = todos.data.listTodos.items;
   },
   methods: {
     async createTodo() {
